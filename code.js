88a51ce50190ed76c9778a7b070d25d14491c56f
raven2290 <robertskody42@gmail.com>
2025-04-07 10:05:50 -0700

    Add files via upload

---
+++ code.js
@@ -0,0 +1,9 @@
+function updateText(){
+    document.getElementById("heading").innerHTML = " ITS OVER 9000!";
+}
+function openINDEX(){
+    document.getElementById("heading").innerHTML = " running system diagnostics...<br>Checking system sta...oops...got it";
+}   
+function openBoards(){
+    document.getElementById("heading").innerHTML = "running system error...<br>Boards are currently down!<br>Please try again later.<br>HELP xP";
+}
