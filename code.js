02255d46604953c98f2839976877b195ffb34846
raven2290 <robertskody42@gmail.com>
2025-04-13 09:54:32 -0700

    Add files via upload

--- code.js
+++ code.js
@@ -1,9 +1,53 @@
+// change context header text
 function updateText(){
     document.getElementById("heading").innerHTML = " ITS OVER 9000!";
 }
-function openINDEX(){
-    document.getElementById("heading").innerHTML = " running system diagnostics...<br>Checking system sta...oops...got it";
-}   
-function openBoards(){
-    document.getElementById("heading").innerHTML = "running system error...<br>Boards are currently down!<br>Please try again later.<br>HELP xP";
+// change home header text
+function linkText(){
+    document.getElementById("home").innerHTML = "this is my home page & thats my meme";
+}
+
+// change the board header text
+function boardText(){
+    document.getElementById("board").innerHTML = "this is my course catolog";
+}
+// play craps
+function playCraps()
+{
+    let results = document.getElementById("crapsResults");
+
+    //generate two random numbers between 1 and 6
+    let die1 = Math.floor(Math.random() * 6) + 1;
+    let die2 = Math.floor(Math.random() * 6) + 1;
+
+    //logs the 2 dice rolls to the console
+    console.log("die1: "+ die1);
+    console.log("die2: "+ die2);
+
+    //sum of the 2 dice rolls
+    let sum = die1 + die2;
+
+    results.innerHTML = "die1: "+ die1 + "<br> die2: "+ die2; //display the 2 dice rolls
+
+    // or operator
+    if (sum == 7 || sum == 11) 
+    {
+        console.log("craps you lose");
+        results.innerHTML = "craps you lose";
+    }
+
+    //and operator
+     else if (die1 == die2 && die1 % 2 == 0)
+        
+    {
+            console.log("you win!");
+            results.innerHTML = "you win!";
+    }
+    else
+    {
+        console.log("pushed!");
+        results.innerHTML = "pushed!";
+    }
+
+    
 }
